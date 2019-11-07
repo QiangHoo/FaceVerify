e8be9033c60378bda798d67ba8f425523bd3769f
QiangHoo <18583861496@163.com>
2019-11-07 16:17:01 +0800

    修改权限验证

--- app/src/main/java/com/arcsoft/arcfacedemo/activity/MainActivity.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/activity/MainActivity.java
@@ -1,20 +1,38 @@
 package com.arcsoft.arcfacedemo.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.arcsoft.arcfacedemo.R;
 import com.arcsoft.arcfacedemo.Tools.SPTools;
+import com.arcsoft.arcfacedemo.common.Constants;
 import com.arcsoft.arcfacedemo.widget.IsFirstVarityDialog;
 
 import com.arcsoft.arcfacedemo.widget.ToastUtil;
+import com.arcsoft.face.ActiveFileInfo;
+import com.arcsoft.face.ErrorInfo;
+import com.arcsoft.face.FaceEngine;
 
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 import static android.view.Window.FEATURE_NO_TITLE;
 
@@ -31,6 +49,12 @@ public class MainActivity extends AppCompatActivity implements IsFirstVarityDial
     private ImageView mIvLockedOrUnLocked;
     private boolean isVerifySuccess = false;
     private boolean isActivityResult = false;
+    private Toast toast;
+    private static final int ACTION_REQUEST_PERMISSIONS = 0x001;
+    private FaceEngine faceEngine = new FaceEngine();
+    private static final String[] NEEDED_PERMISSIONS = new String[]{
+            Manifest.permission.READ_PHONE_STATE
+    };
 
 
     @Override
@@ -50,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements IsFirstVarityDial
             startActivityForResult(intent, REQUEST_CODE);
         }
         initViews();
+        activeEngine();
     }
 
     @Override
@@ -143,6 +168,97 @@ public class MainActivity extends AppCompatActivity implements IsFirstVarityDial
                 break;
             default:
                 break;
+        }
+    }
+
+    /**
+     * 激活引擎
+     *
+     * @param
+     */
+    public void activeEngine() {
+        if (!checkPermissions(NEEDED_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, NEEDED_PERMISSIONS, ACTION_REQUEST_PERMISSIONS);
+            return;
+        }
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                int activeCode = faceEngine.activeOnline(MainActivity.this, Constants.APP_ID,
+                        Constants.SDK_KEY);
+                emitter.onNext(activeCode);
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer activeCode) {
+                        if (activeCode == ErrorInfo.MOK) {
+                            showToast(getString(R.string.active_success));
+                        } else if (activeCode == ErrorInfo.MERR_ASF_ALREADY_ACTIVATED) {
+                            showToast(getString(R.string.already_activated));
+                        } else {
+                            showToast(getString(R.string.active_failed, activeCode));
+                        }
+
+                        ActiveFileInfo activeFileInfo = new ActiveFileInfo();
+                        int res = faceEngine.getActiveFileInfo(MainActivity.this,activeFileInfo);
+                        if (res == ErrorInfo.MOK) {
+                            Log.i(TAG, activeFileInfo.toString());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+    private boolean checkPermissions(String[] neededPermissions) {
+        if (neededPermissions == null || neededPermissions.length == 0) {
+            return true;
+        }
+        boolean allGranted = true;
+        for (String neededPermission : neededPermissions) {
+            allGranted &= ContextCompat.checkSelfPermission(this, neededPermission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return allGranted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == ACTION_REQUEST_PERMISSIONS) {
+            boolean isAllGranted = true;
+            for (int grantResult : grantResults) {
+                isAllGranted &= (grantResult == PackageManager.PERMISSION_GRANTED);
+            }
+            if (isAllGranted) {
+                activeEngine();
+            } else {
+                showToast(getString(R.string.permission_denied));
+            }
+        }
+    }
+    private void showToast(String s) {
+        if (toast == null) {
+            toast = Toast.makeText(this, s, Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            toast.setText(s);
+            toast.show();
         }
     }
 }
